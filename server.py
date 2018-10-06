e1381cf4b422c619be122e7a41d0a2eedbf9ee78
Émilio Gonzalez <egg997@gmail.com>
2018-10-06 09:55:22 -0700

    go home on buy

--- server.py
+++ server.py
@@ -41,6 +41,21 @@ def response():
     print(action)
     return action
 
+app.run(host="0.0.0.0", port=3000, debug=True)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
