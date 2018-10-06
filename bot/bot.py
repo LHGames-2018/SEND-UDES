d4576fc8a704a39687544d2073df6d3803e54312
Guillaume Jean <guillaumejean46@gmail.com>
2018-10-06 12:13:57 -0400

    PEP 8

--- bot/bot.py
+++ bot/bot.py
@@ -14,32 +14,25 @@ handler.setLevel(log_level)
 handler.setFormatter(logging.Formatter("%(asctime)-15s - %(levelname)s - line %(lineno)s - %(funcName)s: %(message)s"))
 log.addHandler(handler)
 
-UP    = Point(0, -1)
-DOWN  = Point(0, 1)
-LEFT  = Point(-1, 0)
+UP = Point(0, -1)
+DOWN = Point(0, 1)
+LEFT = Point(-1, 0)
 RIGHT = Point(1, 0)
 
 tick = -1
 turn_counter = -1
 
+
 class Bot:
     def __init__(self):
-        pass
+        self.player_info = None
+        self.actions = []
 
-    def before_turn(self, playerInfo):
-        """
-        Gets called before ExecuteTurn. This is where you get your bot's state.
-            :param playerInfo: Your bot's current state.
-        """
-        self.PlayerInfo = playerInfo
+    def before_turn(self, player_info):
+        self.player_info = player_info
         self.actions = [GoHome(), GoMine(), Mine()]
 
     def execute_turn(self, game_map: GameMap, visible_players: List[Player]):
-        """
-        This is where you decide what action to take.
-            :param gameMap: The gamemap.
-            :param visiblePlayers:  The list of visible players.
-        """
         grid = Grid(30000, 30000)
         for column in game_map.tiles:
             for t in column:
@@ -49,12 +42,12 @@ class Bot:
         the_best_action: ActionTemplate = None
         log.info("Determining best action: {}".format(the_best_action))
         for action in self.actions:
-            weight = action.calculate_weight(self.PlayerInfo, game_map, visible_players)
+            weight = action.calculate_weight(self.player_info, game_map, visible_players)
             if weight > biggest_weight:
                 biggest_weight = weight
                 the_best_action = action
         log.info("Best action: {}".format(the_best_action))
-        return the_best_action.get_action(self.PlayerInfo, game_map, visible_players, grid)
+        return the_best_action.get_action(self.player_info, game_map, visible_players, grid)
 
     def get_mine_position(self):
         return None
