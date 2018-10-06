248933bac4844a7ae8699141992a34df5e021c77
Alexandre <lara3407@usherbrooke.ca>
2018-10-06 11:54:04 -0400

    Basic actions

--- bot/actions.py
+++ bot/actions.py
@@ -23,7 +23,12 @@ class ActionTemplate:
 class GoMine(ActionTemplate):
 
     def calculate_weight(self, player_info: Player, game_map: GameMap, visible_players: List[Player]):
-        return 1
+        calculated_weight = 0
+
+        if player_info.CarriedResources == 0:
+            calculated_weight = 1
+
+        return calculated_weight
 
     def get_action(self, player_info: Player, game_map: GameMap, visible_players: List[Player]):
         return create_move_action(UP)
@@ -32,7 +37,15 @@ class GoMine(ActionTemplate):
 class Mine(ActionTemplate):
 
     def calculate_weight(self, player_info: Player, game_map: GameMap, visible_players: List[Player]):
-        return 0
+        calculated_weight = 0
+
+        if game_map.getTileAt((player_info.Position.x - 1, player_info.Position.y)) == TileContent.Resource\
+                or game_map.getTileAt((player_info.Position.x + 1, player_info.Position.y)) == TileContent.Resource\
+                or game_map.getTileAt((player_info.Position.x, player_info.Position.y - 1)) == TileContent.Resource\
+                or game_map.getTileAt((player_info.Position.x, player_info.Position.y + 1)) == TileContent.Resource:
+            calculated_weight = 1
+
+        return calculated_weight
 
     def get_action(self, player_info: Player, game_map: GameMap, visible_players: List[Player]):
         return create_collect_action(UP)
@@ -41,7 +54,14 @@ class Mine(ActionTemplate):
 class GoHome(ActionTemplate):
 
     def calculate_weight(self, player_info: Player, game_map: GameMap, visible_players: List[Player]):
-        return 0
+        calculated_weight = 0
+
+        if player_info.CarriedResources == player_info.CarryingCapacity:
+            calculated_weight = 1
+
+        return calculated_weight
 
     def get_action(self, player_info: Player, game_map: GameMap, visible_players: List[Player]):
+
+
         return create_move_action(UP)
